d8c0cd0e9eb567e3a0d3e8415dc1c835d00d2957
edwightdelgado <edwightdelgado@gmail.com>
2021-08-16 09:32:01 -0500

    python intermedio(platzi) - listas-y-diccionarios-anidados lista

--- code/lists_and_dicts.py
+++ code/lists_and_dicts.py
@@ -1,4 +1,5 @@
 def main():
+	""" lists and dicts """
 	my_list=[1,'verdad',True,4.5]
 	my_dict={'firstname':'edwight','lastname':'delgado'}
 
@@ -12,7 +13,18 @@ def main():
 		'integer_nums':[-5,-4,-3,-2],
 		'floting_nums':[2.3,5.6,-2.8],
 	}
+	print('super dicts')
 	for key,values in superdict.items():
 		print(key, values)
+
+	print('super list 1')
+	for lista in superlist:
+		print('firstname: ',lista['firstname'],', lastname: ',lista['lastname'])
+
+	print('super list 2')
+	for lista in superlist:
+		for key, value in lista.items():
+			print(f'{key},{value}')
+
 if __name__ == '__main__':
 	main()
